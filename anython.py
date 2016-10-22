f0a99f84ef11871cbb21ecaa2c7f754cf2c7b8d0
Alexander Pushkov <hi@ale.rocks>
2016-10-22 15:36:43 +0300

    Make it a generator

--- anython.py
+++ anython.py
@@ -1,37 +1,44 @@
-class AnythonIndentationError(IndentationError):
+class TwtIndentationError(ValueError):
     pass
 
-with open("test.icss") as fi:
+
+def twt(lines):
+    """
+    Takes an iterable of lines (ignores line endings) and does its magic.
+
+    Yields lines (no line endings).
+    """
+
     current_indents = [0]
     new_block = False
 
-    for line in fi.readlines():
+    for line in lines:
         if "\t" in line:
-            raise AnythonIndentationError("no tabs allowed")
+            raise TwtIndentationError("no tabs allowed")
 
         new_indent = len(line) - len(line.lstrip())
         line = line.rstrip()
 
         if line.strip() == "":
-            print("")
+            yield ""
             continue
 
         if new_block:
             if new_indent <= current_indents[-1]:
-                raise AnythonIndentationError("expected an indented block")
+                raise TwtIndentationError("expected an indented block")
             current_indents.append(new_indent)
             new_block = False
 
         while new_indent < current_indents[-1]:
             current_indents.pop()
-            print((" " * current_indents[-1]) + "}")
+            yield (" " * current_indents[-1]) + "}"
 
         if line.endswith(":"):
             line = line.rstrip(":") + " {"
             new_block = True
 
-        print(line)
+        yield line
 
     while 0 < current_indents[-1]:
         current_indents.pop()
-        print((" " * current_indents[-1]) + "}")
+        yield (" " * current_indents[-1]) + "}"
